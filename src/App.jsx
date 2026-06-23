af8fc0a033cfee47766dac326242f20566624006
Yasir Rajput <rajputyasir2005@gmail.com>
2026-06-24 00:56:25 +0530

    fix(app): add missing dispatch dependency to useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import './App.css'
-import authService from "./appwrite/auth"
-import {login, logout} from "./store/authSlice"
-import { Footer, Header } from './components'
-import { Outlet } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [loading, setLoading] = useState(true)
-  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    authService.getCurrentUser()
-    .then((userData) => {
-      if (userData) {
-        dispatch(login({userData}))
-      } else {
-        dispatch(logout())
-      }
-    })
-    .finally(() => setLoading(false))
-  }, [])
-  
+    // Fetch the current logged-in user session on component mount
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false)); // Ensure loading state is turned off
+  }, [dispatch]); // Added 'dispatch' to dependency array to fix exhaustive-deps warning
+
   return !loading ? (
-    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
-      <div className='w-full block'>
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
         <Header />
         <main>
-        TODO:  <Outlet />
+          TODO: <Outlet />
         </main>
         <Footer />
       </div>
     </div>
-  ) : null
+  ) : null;
 }
 
-export default App
+export default App;
