c6aa06ceec78d837b5aed4bf084f6baeca88b98a
Yasir Rajput <rajputyasir2005@gmail.com>
2026-06-24 14:15:13 +0530

    chore(router): fix react-router-dom future flag warning in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,86 +1,88 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store/store.js'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import { AuthLayout, Login } from './components/index.js'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import { AuthLayout, Login } from "./components/index.js";
 
 import AddPost from "./pages/AddPost";
-import Signup from './pages/Signup'
+import Signup from "./pages/Signup";
 import EditPost from "./pages/EditPost";
-
 import Post from "./pages/Post";
-
 import AllPosts from "./pages/AllPosts";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/login",
+          element: (
+            <AuthLayout authentication={false}>
+              <Login />
+            </AuthLayout>
+          ),
+        },
+        {
+          path: "/signup",
+          element: (
+            <AuthLayout authentication={false}>
+              <Signup />
+            </AuthLayout>
+          ),
+        },
+        {
+          path: "/all-posts",
+          element: (
+            <AuthLayout authentication={true}>
+              <AllPosts />
+            </AuthLayout>
+          ),
+        },
+        {
+          path: "/add-post",
+          element: (
+            <AuthLayout authentication={true}>
+              <AddPost />
+            </AuthLayout>
+          ),
+        },
+        {
+          path: "/edit-post/:slug",
+          element: (
+            <AuthLayout authentication={true}>
+              <EditPost />
+            </AuthLayout>
+          ),
+        },
+        {
+          path: "/post/:slug",
+          element: <Post />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-        {
-            path: "/",
-            element: <Home />,
-        },
-        {
-            path: "/login",
-            element: (
-                <AuthLayout authentication={false}>
-                    <Login />
-                </AuthLayout>
-            ),
-        },
-        {
-            path: "/signup",
-            element: (
-                <AuthLayout authentication={false}>
-                    <Signup />
-                </AuthLayout>
-            ),
-        },
-        {
-            path: "/all-posts",
-            element: (
-                <AuthLayout authentication>
-                    {" "}
-                    <AllPosts />
-                </AuthLayout>
-            ),
-        },
-        {
-            path: "/add-post",
-            element: (
-                <AuthLayout authentication>
-                    {" "}
-                    <AddPost />
-                </AuthLayout>
-            ),
-        },
-        {
-            path: "/edit-post/:slug",
-            element: (
-                <AuthLayout authentication>
-                    {" "}
-                    <EditPost />
-                </AuthLayout>
-            ),
-        },
-        {
-            path: "/post/:slug",
-            element: <Post />,
-        },
-    ],
-},
-])
+    // Opt-in early to the React Router v7 startTransition feature flag
+    future: {
+      v7_startTransition: true,
+    },
+  },
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-    <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
-)
+);
