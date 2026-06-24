4ea2ac0d25ae4accbefa6c8eadb736ae9451ddd1
Yasir Rajput <rajputyasir2005@gmail.com>
2026-06-24 13:52:19 +0530

    fix(accessibility): link label text with select control in Select component

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,29 +1,29 @@
-import React, {useId} from 'react'
+import React, { useId } from "react";
 
-function Select({
-    options,
-    label,
-    className,
-    ...props
-}, ref) {
-    const id = useId()
+function Select({ options, label, className, ...props }, ref) {
+  const id = useId();
   return (
-    <div className='w-full'>
-        {label && <label htmlFor={id} className=''></label>}
-        <select
+    <div className="w-full">
+      {/* Render the label text inside the tag to fix the accessibility bug */}
+      {label && (
+        <label htmlFor={id} className="inline-block mb-1 pl-1">
+          {label}
+        </label>
+      )}
+      <select
         {...props}
         id={id}
         ref={ref}
         className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
-        >
-            {options?.map((option) => (
-                <option key={option} value={option}>
-                    {option}
-                </option>
-            ))}
-        </select>
+      >
+        {options?.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
     </div>
-  )
+  );
 }
 
-export default React.forwardRef(Select)
+export default React.forwardRef(Select);
