b187f8c06c6b353426db1150118db398818cdfa3
Yasir Rajput <rajputyasir2005@gmail.com>
2026-08-21 16:25:30 +0530

    fix(container): add propTypes validation to Container

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,8 +1,13 @@
+import PropTypes from "prop-types";
 
 function Container({ children }) {
   return (
     <div className="w-full max-w-6xl mx-auto px-4 sm:px-6">{children}</div>
   );
 }
+
+Container.propTypes = {
+  children: PropTypes.node,
+};
 
 export default Container;
